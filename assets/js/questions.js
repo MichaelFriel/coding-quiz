52c80be407ff6e68198982ce42e2faf9211d1309
Michael Friel <mikefriel92@hotmail.com>
2024-01-12 18:35:20 +0000

    Added an additional js file for scores and added ten questions to the questions.js file.

---
+++ assets/js/questions.js
@@ -0,0 +1,53 @@
+const questions = [
+    {title: "Which symbol is used for comments in JavaScript?",
+    choices: ["A) //", "B) /* */", "C) <!-- -->", "D) Both A and B"],
+    answer: "D) Both A and B"
+    },
+
+    {title: "Which of these is a new semantic element in HTML5?",
+    choices: ["A) <div>","B) <span>","C) <article","D) <bold>"],
+    answer: "C) <article>"
+    },
+
+    {title: "SASS is an example of what?",
+    choices: ["A) CSS Preprocessor","B) Javascript Framework","C) HTML Template Engine","D) Web Server"],
+    answer: "A) CSS Preprocessor"
+    },
+
+    {title: "Which of the following is a way to store data localls in a user's browser?",
+    choices: ["A) Cookies","B) Session Storage","C) Local Storage","D) Both A & C","E) A, B & C"],
+    answer: ""
+    },
+
+    {title: "What is the main purpose of minifying CSS and javascript files?",
+    choices: ["A) To reduce file size for faster loading","","",""],
+    answer: ""
+    },
+
+    {title: "XXX",
+    choices: ["","","",""],
+    answer: ""
+    },
+
+    {title: "XXX",
+    choices: ["","","",""],
+    answer: ""
+    },
+
+    {title: "XXX",
+    choices: ["","","",""],
+    answer: ""
+    },
+
+    {title: "XXX",
+    choices: ["","","",""],
+    answer: ""
+    },
+
+    {title: "XXX",
+    choices: ["","","",""],
+    answer: ""
+    },
+
+    
+]
