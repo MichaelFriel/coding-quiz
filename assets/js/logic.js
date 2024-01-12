19ff058c88541c639872e2c72bcfde598b901928
Michael Friel <mikefriel92@hotmail.com>
2024-01-12 16:23:21 +0000

    Added two JS files and included some initial plans to approach the challenge.

---
+++ assets/js/logic.js
@@ -0,0 +1,19 @@
+
+
+// Create questions and answer 
+// Find all of the required elements through query selectors.
+// Write function which creates h2 elements and divs with choices. It should also display answer.
+// For loop which goes through each question until time has run out.
+// Empty content and then add the next question.
+// Log score to local storage (variable which is added to)
+// Pull this information from local storage and add to highscores page.
+// Set timer which increments down by 1 every 1000ms. 
+// Remove 10 from it every time a question is answered incorrectly.
+// End the game when the timer variable hits 0.
+// Redirect to the highscores page once this is completed.
+// Add click event to the clear button, which empties content from local storage.
+
+// NICE TO HAVE
+// Timer to go up by 5 seconds with a correct answer
+// Add more questions (50-100) and randomise the chosen questions each time, for longevity of game.
+// Timer to turn red when the user has less than 10 seconds left (when var < 10, style:red)
