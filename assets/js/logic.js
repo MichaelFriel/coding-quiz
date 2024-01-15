66dae15cdd6120a4f5f928b330f9493326790c47
Michael Friel <mikefriel92@hotmail.com>
2024-01-15 12:39:22 +0000

    Added userDetails to local storage,which includes their initials and their score.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,6 +8,8 @@ const quizButtonsEl = document.querySelector('.questionChoices');
 const endScreenEl = document.querySelector('#end-screen');
 const finalScoreEl = document.querySelector('#final-score');
 const feedbackEl = document.querySelector('#feedback');
+const submitButtonEl = document.querySelector('#submit');
+const userInitialsEl = document.querySelector('#initials')
 
 // Variables
 let currentQuestionIndex = 0;
@@ -132,33 +134,17 @@ function addQuestion (question) {
 
     startQuizButtonEl.addEventListener("click", startQuiz)
 
-//     console.log(questionContainerEl);
+// Event Listener and function for button submit.
 
-    
+submitButtonEl.addEventListener("click", function (event) {
+event.preventDefault();
 
-    // const questionChoicesContainer = document.createElement("div");
-    // questionChoices.textContent = question.choices
+let userDetails = {
+    initials: userInitialsEl.value,
+    score: score
+};
 
-
-// Create questions and answer - Done
-// Find all of the required elements through query selectors. - Done
-// Write function which creates h2 elements and divs with choices. It should also display answer. - Done
-// For loop which goes through each question until time has run out.
-// Empty content and then add the next question.
-// Log score to local storage (variable which is added to)
-// Pull this information from local storage and add to highscores page.
-// Set timer which increments down by 1 every 1000ms. - Done
-// Remove 10 from it every time a question is answered incorrectly.
-// End the game when the timer variable hits 0.
-// Redirect to the highscores page once this is completed.
-// Add click event to the clear button, which empties content from local storage.
-
-// NICE TO HAVE
-// Timer to go up by 5 seconds with a correct answer
-// Add more questions (50-100) and randomise the chosen questions each time, for longevity of game.
-// Timer to turn red when the user has less than 10 seconds left (when var < 10, style:red)
+localStorage.setItem("score",JSON.stringify(userDetails));
+})
 
 
-
-// Thoughts
-// Add a Current Question Inde
